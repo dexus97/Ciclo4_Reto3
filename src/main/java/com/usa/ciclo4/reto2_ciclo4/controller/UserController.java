12832c535b4786023851f5a8dc1d685dbab2412e
Axus97 Romero <aresramirez57@gmail.com>
2021-12-15 18:20:45 -0500

    r

--- src/main/java/com/usa/ciclo4/reto2_ciclo4/controller/UserController.java
+++ src/main/java/com/usa/ciclo4/reto2_ciclo4/controller/UserController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * @author jonathan  romero
+ */
 @RestController
 @RequestMapping("/api/user/")
 @CrossOrigin(origins = "*", methods = {RequestMethod.GET,RequestMethod.POST,RequestMethod.PUT,RequestMethod.DELETE})
